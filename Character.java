d14366af44b6276afebf0c9607eabd5049ceba8d
emilyliao97 <liao.emily022@gmail.com>
2014-01-11 13:08:27 -0500

    update instance vars

---
+++ Character.java
@@ -0,0 +1,12 @@
+public class Character {
+
+  protected int _happiness;
+  protected double _moolah;
+  protected int _tileCtr;
+  protected int _strength;
+  protected int _smarts;
+  protected int _mouthMeter;
+  protected int _interest;
+  protected boolean _hasMet;
+
+}
