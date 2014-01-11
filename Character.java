70c0791ebff302fc6a3cb9b65eb7e4d0f4d22252
Kotisheu <iiacherry@aim.com>
2014-01-11 13:47:53 -0500

    Update Character.java

--- Character.java
+++ Character.java
@@ -1,14 +1,16 @@
 public class Character {
 
-  protected int _happiness;
+  protected int _happiness;//need shorter name
   protected double _moolah;
   protected int _tileCtr;
-  protected int _strength;
-  protected int _smarts;
-  protected int _mouthMeter;
-  protected int _interest;
+  protected int _str;//strength
+  protected int _int;//intelligence
+  protected int _mouthMeter;// need shorter name
+  protected int _interest;// need shorter name
   protected boolean _hasMet;
-  protected int _attractiveness;
+  protected int _att;//attractiveness
   protected String _name;
+  protected int _hp;//health, needed to perform certain activities
+  
 
 }
