8168ad176f1c5cfa8453fd6d6be0e60b840089f5
Kotisheu <iiacherry@aim.com>
2014-01-15 15:30:54 -0500

    Create afterHours.java

---
+++ afterHours.java
@@ -0,0 +1,10 @@
+public class afterHours{
+//interface for each place
+// interest method
+//etc.
+//each place- chances of meeting other character, dialouge per character, effects
+public void(?) choosePlace(){
+//choose place, goes to that class, performs place-specific events
+}
+
+}
