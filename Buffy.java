b17aa196a7a63e1a73d23c858491109868ba63c2
emilyliao97 <liao.emily022@gmail.com>
2014-01-15 14:42:19 -0500

    Update Buffy.java

--- Buffy.java
+++ Buffy.java
@@ -1,6 +1,8 @@
 public class Buffy extends Character { 
 
   public Buffy(Player a) {
+    
+    // set gender of the Buff person opposite of the player
     if ( a.getGen() == true ) {
       setGen(false);
       setName("Luke");
@@ -9,7 +11,16 @@ public class Buffy extends Character {
       setGen(true);
       setName("Mia");
     }
-    //figure out the rest of the attributes
+    
+    // set each of the attributes to a random number between 0 and 25
+    setInt( (int) 25 * Math.random() );
+    setMo( (int) 25 * Math.random() );
+    setAtt( (int) 25 * Math.random() );
+    // Remember: the intelligence of the nerd is higher than 50
+    setStr( 50 + (int) ( 50 * Math.random() ) );
+    // initially, you have not met, so hasMet = false, & affinity(interest) is therefore 0
+    setMet("false");
+    setAff(0);
   }
 
 }
