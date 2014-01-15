dfbc2383605248a5a1f100ef83d47bddbacbfd64
emilyliao97 <liao.emily022@gmail.com>
2014-01-15 14:44:09 -0500

    Update Pretty.java

--- Pretty.java
+++ Pretty.java
@@ -1,6 +1,8 @@
 public class Pretty extends Character { 
 
   public Pretty(Player a) {
+    
+    // set the gender of the pretty person opposite the player
     if ( a.getGen() == true ) {
       setGen(false);
       setName("Damon");
@@ -9,7 +11,16 @@ public class Pretty extends Character {
       setGen(true);
       setName("Vivian");
     }
-    //figure out the rest of the attributes
+    
+    // set each of the attributes to a random number between 0 and 25
+    setStr( (int) 25 * Math.random() );
+    setMo( (int) 25 * Math.random() );
+    setInt( (int) 25 * Math.random() );
+    // Remember: the intelligence of the nerd is higher than 50
+    setAtt( 50 + (int) ( 50 * Math.random() ) );
+    // initially, you have not met, so hasMet = false, & affinity(interest) is therefore 0
+    setMet("false");
+    setAff(0);
   }
 
 }
