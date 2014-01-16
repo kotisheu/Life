dc28b11ebe3c048bd1142b1b75daef49f8452a0b
emilyliao97 <liao.emily022@gmail.com>
2014-01-16 14:26:17 -0500

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,6 +1,8 @@
 public class Student extends Character { 
 
   public Student(Player a) {
+    
+    // set the Student as the opposite gender of the Player
     if ( a.getGen() == true ) {
       setGen(false);
       setName("Sebastian");
@@ -9,7 +11,16 @@ public class Student extends Character {
       setGen(true);
       setName("Alyssa");
     }
-    //figure out the rest of the attributes
+    
+    // school is at beginning of game -- lower stats required
+    setInt( (int) ( 25 * Math.random() ) );
+    setMo( (int) ( 25 * Math.random() ) );
+    setAtt( (int) ( 25 * Math.random() ) );
+    setStr( (int) ( 25 * Math.random() ) );
+    // initially, you have not met, so hasMet = false, & affinity(interest) is therefore 0
+    setMet("false");
+    setAff(0);
+    
   }
 
 }
