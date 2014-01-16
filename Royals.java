12a4dfe63f3ee8af395525f291579bfc58d3467a
emilyliao97 <liao.emily022@gmail.com>
2014-01-16 14:17:53 -0500

    Update Royals.java

--- Royals.java
+++ Royals.java
@@ -1,6 +1,8 @@
 public class Royals extends Character { 
 
   public Royals(Player a) {
+    
+    // set gender opposite that of player
     if ( a.getGen() == true ) {
       setGen(false);
       setName("Will");
@@ -9,7 +11,15 @@ public class Royals extends Character {
       setGen(true);
       setName("Tatiana");
     }
-    //figure out the rest of the attributes
+    
+    // set each of the attributes to a random number between 75 and 100 because they're royalty-- super high stats
+    setInt( 75 + (int) ( 25 * Math.random() ) );
+    setMo( 75+ (int) ( 25 * Math.random() ) );
+    setAtt( 75 + (int) ( 25 * Math.random() ) );
+    setStr( 75 + (int) ( 25 * Math.random() ) );
+    // initially, you have not met, so hasMet = false, & affinity(interest) is therefore 0
+    setMet("false");
+    setAff(0);
   }
 
 }
