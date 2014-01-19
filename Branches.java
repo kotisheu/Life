63591890081009e0ec8a78868bc945720d67b33d
Kotisheu <iiacherry@aim.com>
2014-01-19 11:09:08 -0500

    Update Branches.java

--- Branches.java
+++ Branches.java
@@ -10,42 +10,42 @@ public class Branches{
      a.setRoute(D);
      }
      
-  public void child1(person a){ //tile2 routeAC/BC preReq still married,interest >60 
+  public void child1(person a){ //tile235 routeAC/BC preReq still married,interest >60 
     /*choice1-yes
     a.setRoute(E);
     */choice2-no
      a.setRoute(F);
      }
      
-     public void house(person a){ //tile routeACE/ACF/BCE/BCF preReq money> ???
+     public void house(person a){ //tile265/330 routeACE/ACF/BCE/BCF preReq money> ???
     /*choice1-yes
     a.setRoute(G);
     */choice2-nom
      a.setRoute(H);
      }
      
-     public void child2(person a){ //tile routeACEG/ACFG/BCEG/BCFG/ADKO/ADKP/ADLM/ADLN/BDKO/BDKP/BDLM/BDLN preReq stillmarried, interest > 70
+     public void child2(person a){ //tile295 routeACEG/ACFG/BCEG/BCFG/ADKO/ADKP/ADLM/ADLN/BDKO/BDKP/BDLM/BDLN preReq stillmarried, interest > 70
     /*choice1-yes
     a.setRoute(I);
     */choice2-no
      a.setRoute(J);
      }
      
-     public void car(person a){ //tile routeAD/BD/ACEGI/ACEGJ/ACEH/ACFGI/ACFGJ/ACFH preReq money>??? 
+     public void car(person a){ //tile320/235 routeAD/BD/ACEGI/ACEGJ/ACEH/ACFGI/ACFGJ/ACFH preReq money>??? 
     /*choice1-yes
     a.setRoute(K);
     */choice2-no
      a.setRoute(L);
      }
      
-     public void business(person a){ //tile routeADL/BDL/ACEGI/ACEGJK/ACEHK/ACFGIK/ACFGJK/ACFHK/ACEGIL/ACEGJL/ACEHL/ACFGIL/ACFGJL/ACFHL preReq 
+     public void business(person a){ //tile345/255 routeADL/BDL/ACEGI/ACEGJK/ACEHK/ACFGIK/ACFGJK/ACFHK/ACEGIL/ACEGJL/ACEHL/ACFGIL/ACFGJL/ACFHL preReq $$>
     /*choice1-yes
     a.setRoute(M);
     */choice2-no
      a.setRoute(N);
      }
      
-     public void travel(person a){ //tile routeADK/BDK preReq 
+     public void travel(person a){ //tile routeADK/BDK preReq $$>??
     /*choice1-yes
     a.setRoute(O);
     */choice2-no
