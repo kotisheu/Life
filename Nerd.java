fe9e91db2177cbc75969cec2354ca826f8beac34
emilyliao97 <liao.emily022@gmail.com>
2014-01-19 16:13:37 -0500

    Update Nerd.java

--- Nerd.java
+++ Nerd.java
@@ -25,7 +25,7 @@ public class Nerd extends Character {
   
   public void talk() {
     String r = "";
-    r = "I thought I'd take a step outside... The world really is wonderous...";
+    r = "The world really is wonderous, as in the books...";
     System.out.println(r);
   }
 
