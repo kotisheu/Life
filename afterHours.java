03ffed39968527891c161ce330dbb1d564dc9fb9
emilyliao97 <liao.emily022@gmail.com>
2014-01-19 16:33:47 -0500

    Update afterHours.java

--- afterHours.java
+++ afterHours.java
@@ -1,40 +1,149 @@
 public class afterHours{
-//interface for each place
-// interest method
-//etc.
-//each place- chances of meeting other character, dialouge per character, effects
-//choose place, goes to that class, performs place-specific events
-public afterHours(){
-}
-//from highest chance to lowest chance
-public void school(){
-  //(+int, -hp, +mm),only if route A-> Student,
-}
-public void gym(){ 
-  //(++str, -$),buffy,pretty,work
-}
-public void library (){
-//(++int),smart,student(unable to raise interest if not route A)
-}
-public void mall(){
-//(+att , --$)pretty, work, buffy
-}
-public void beautySalon(){
-//(++att, -$) pretty, smart
-}
-public void work(){
-//( ++$, -happi, -hp),workaholic
-}
-
-public void walk(){
-//(+happi, +hp),none,royal
-}
-public void date(){
-//(--$, ++happi), choose who, names are  generic if havent met( ie, Girl1, Girl2, Emily, Girl4,Bob)
-// if interest if higher than x amount, they accept, Causes + interest depending on how it goes
-}
-// interest is raised by a) having higer stats than the character(+ 25(?)), 
-//b. meeting the character more often(non-date) (-3 to +3 , depending on the dialogue),
-//and C. Asking them on a date(+/- 1/5/10, depending on calibur of date, aka randomly :D
+  //interface for each place
+  // interest method
+  //etc.
+  //each place- chances of meeting other character, dialouge per character, effects
+  //choose place, goes to that class, performs place-specific events
+  
+  public afterHours(Player A){
+    Student stu = new Student(A);
+    Buffy buf = new Buffy(A);
+    Nerd ner = new Nerd(A);
+    Pretty pret = new Pretty(A);
+    Workaholic work = new Workaholic(A);
+    Royal roy = new Royal(A);
+  }
+  //from highest chance to lowest chance
+  
+  public void school(Player A){
+    //(+int, -hp, +mm),only if route A-> Student,
+    A.setInt( A.getInt() + 5 );
+    A.setHP( A.getHP() - 2 );
+    A.setMo( A.getMo() + 5 );
+    if (A.getRoute() == "A") {
+      int boo = (int) ( 5 * Math.random() );
+      if ( boo == 0 && stu.getMet() == false ) {
+        stu.setMet(true);
+        stu.firstTalk();
+      }
+      else if ( boo == 0 && stu.getMet() = true )
+        stu.talk();
+        stu.setAff( pret.getAff() + 5 );
+    }
+  }
+  
+  public void gym(Player A){ 
+    //(++str, -$),buffy,pretty,work
+    A.setStr( A.getStr() + 5 );
+    A.setMoo( A.getMoo() - 10 );
+    int boo = (int) ( 5 * Math.random() );
+    if ( boo == 0 && buf.getMet() == false ) {
+      buf.setMet(true);
+      buf.firstTalk();
+    }
+    else if ( boo == 0 && buf.getMet() = true ) {
+      buf.talk();
+      buf.setAff( pret.getAff() + 5 );
+    }
+    else if ( (int) ( 15 * Math.random() ) == 0  ) {
+      pret.talk();
+      pret.setAff( pret.getAff() + 1 );
+    }
+    else if ( (int) ( 25 * Math.random() ) == 0  ) {
+      work.talk();
+      work.setAff( pret.getAff() + 1 );
+    }
+  }
+  
+  public void library (Player A){
+  //(++int),smart,student(unable to raise interest if not route A)
+    A.setInt( A.getInt() + 5 );
+    int boo = (int) ( 5 * Math.random() );
+    if ( boo == 0 && ner.getMet() == false ) {
+      ner.setMet(true);
+      ner.firstTalk();
+    }
+    else if ( boo == 0 && ner.getMet() = true ) {
+      ner.talk();
+      ner.setAff( pret.getAff() + 5 );
+    }
+    else if ( (int) ( 15 * Math.random() ) == 0 && A.getRoute() == "A" ) {
+      stu.talk();
+      stu.setAff( pret.getAff() + 1 );
+    }
+  }
+  
+  public void mall(Player A){
+  //(+att , --$)pretty, work, buffy
+    A.setAtt( A.getAtt() + 5 );
+    A.setMoo( A.getMoo() - 10 );
+    int boo = (int) ( 5 * Math.random() );
+    if ( boo == 0 && pret.getMet() == false ) {
+      pret.setMet(true);
+      pret.firstTalk();
+    }
+    else if ( boo == 0 && pret.getMet() = true ) {
+      pret.talk();
+      pret.setAff( pret.getAff() + 5 );
+    }
+    else if ( (int) ( 15 * Math.random() ) == 0  ) {
+      work.talk();
+      work.setAff( pret.getAff() + 1 );
+    }
+    else if ( (int) ( 25 * Math.random() ) == 0  ) {
+      buf.talk();
+      buf.setAff( pret.getAff() + 1 );
+    }
+  }
+  
+  public void beautySalon(Player A){
+  //(++att, -$) pretty, smart
+    A.setAtt( A.getStr() + 5 );
+    A.setMoo( A.getMoo() - 10 );
+    int boo = (int) ( 10 * Math.random() );
+    if ( boo == 0 ) {
+      pret.talk();
+      pret.setAff( pret.getAff() + 1 );
+    }
+    else if ( (int) ( 25 * Math.random() ) == 0  ) {
+      ner.talk();
+      ner.setAff( ner.getAff() + 1 );
+    }
+  }
+  public void work(Player A){
+  //( ++$, -happi, -hp),workaholic
+    A.setStr( A.getStr() + 5 );
+    A.setMoo( A.getMoo() - 10 );
+    int boo = (int) ( 10 * Math.random() );
+    if ( boo == 0 && work.getMet() == false ) {
+      work.setMet(true);
+      work.firstTalk();
+    }
+    else if ( boo == 0 && work.getMet() = true ) {
+      work.talk();
+      work.setAff( pret.getAff() + 5 );
+    }
+  }
+  
+  public void walk(Player A){
+  //(+happi, +hp),none,royal
+    A.setStr( A.getStr() + 5 );
+    A.setMoo( A.getMoo() - 10 );
+    int boo = (int) ( 100 * Math.random() );
+    if ( boo == 0 && roy.getMet() == false ) {
+      roy.setMet(true);
+      roy.firstTalk();
+    }
+    else if ( boo == 0 && roy.getMet() = true ) {
+      roy.talk();
+      roy.setAff( pret.getAff() + 2 );
+  }
+  public void date(){
+  //(--$, ++happi), choose who, names are  generic if havent met( ie, Girl1, Girl2, Emily, Girl4,Bob)
+  // if interest if higher than x amount, they accept, Causes + interest depending on how it goes
+  }
+  // interest is raised by a) having higer stats than the character(+ 25(?)), 
+  //b. meeting the character more often(non-date) (-3 to +3 , depending on the dialogue),
+  //and C. Asking them on a date(+/- 1/5/10, depending on calibur of date, aka randomly :D
 
 }
