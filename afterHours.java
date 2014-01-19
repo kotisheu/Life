06a413bb20767f67e2b82c6ffc6e2af449a61b6b
Kotisheu <iiacherry@aim.com>
2014-01-18 22:18:43 -0500

    Update afterHours.java

--- afterHours.java
+++ afterHours.java
@@ -3,8 +3,38 @@ public class afterHours{
 // interest method
 //etc.
 //each place- chances of meeting other character, dialouge per character, effects
-public void(?) choosePlace(){
 //choose place, goes to that class, performs place-specific events
+public afterHours(){
 }
+//from highest chance to lowest chance
+public void school(){
+  //(+int, -hp, +mm),only if route A-> Student,
+}
+public void gym(){ 
+  //(++str, -$),buffy,pretty,work
+}
+public void library (){
+//(++int),smart,student(unable to raise interest if not route A)
+}
+public void mall(){
+//(+att , --$)pretty, work, buffy
+}
+public void beautySalon(){
+//(++att, -$) pretty, smart
+}
+public void work(){
+//( ++$, -happi, -hp),workaholic
+}
+
+public void walk(){
+//(+happi, +hp),none,royal
+}
+public void date(){
+//(--$, ++happi), choose who, names are  generic if havent met( ie, Girl1, Girl2, Emily, Girl4,Bob)
+// if interest if higher than x amount, they accept, Causes + interest depending on how it goes
+}
+// interest is raised by a) having higer stats than the character(+ 25(?)), 
+//b. meeting the character more often(non-date) (-3 to +3 , depending on the dialogue),
+//and C. Asking them on a date(+/- 1/5/10, depending on calibur of date, aka randomly :D
 
 }
