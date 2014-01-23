ed04e0d46152bd122ace0a77da34e55d22a60e39
emilyliao97 <liao.emily022@gmail.com>
2014-01-23 14:44:55 -0500

    Update afterHours.java

--- afterHours.java
+++ afterHours.java
@@ -9,11 +9,15 @@ public class afterHours{
   }
   //from highest chance to lowest chance
   
-  public void school(Player A, Student stu){
+  public Sting school(Player A, Student stu){
     //(+int, -hp, +mm),only if route A-> Student,
+    String retStr = "";
     A.setInt( A.getInt() + 5 );
+    retStr += "School is good for the brain! +5 Intelligence";
     A.setHP( A.getHP() - 2 );
+    retStr += "\nSchool = no sleep + stress = -2 HP";
     A.setMo( A.getMo() + 5 );
+    retStr += "\nArguing with teachers and classmates makes you more apt at talking. +5 Mouth meter";
     if (A.getRoute() == "A") {
       int boo = (int) ( 5 * Math.random() );
       if (( boo == 0) &&( stu.getMet() == false )) {
@@ -21,83 +25,107 @@ public class afterHours{
 	// stu.firstTalk();
       }
       else if (( boo == 0 )&&( stu.getMet()== true ))
-        stu.talk();
+        retStr += "\n" + stu.talk();
         stu.setAff( stu.getAff() + 5 );
+        retStr += "\n+5 Affinity!";
     }
+    return retStr;
   }
   
-  public void gym(Player A, Buffy buf, Pretty pret, Workaholic work){ 
+  public String gym(Player A, Buffy buf, Pretty pret, Workaholic work){ 
     //(++str, -$),buffy,pretty,work
+    String retStr = "";
     A.setStr( A.getStr() + 5 );
+    retStr += "Working out... what doesn't kill you makes you stronger! +5 Strength";
     A.setMoo( A.getMoo() - 10 );
+    retStr += "\n...Gym membership isn't free, ya know! - $10";
     int boo = (int) ( 5 * Math.random() );
     if ( (boo == 0 )&& (buf.getMet() == false )) {
       buf.setMet(true);
       // buf.firstTalk();
     }
     else if ( (boo == 0) &&( buf.getMet()== true) ) {
-      buf.talk();
+      retStr += "\n" + buf.talk();
       buf.setAff( buf.getAff() + 5 );
+      retStr += "\n+5 Affinity!";
     }
     else if ( (int) ( 15 * Math.random() ) == 0  ) {
-      pret.talk();
+      retStr += "\n" + pret.talk();
       pret.setAff( pret.getAff() + 1 );
+      retStr += "\n+1 Affinity!";
     }
     else if ( (int) ( 25 * Math.random() ) == 0  ) {
-      work.talk();
+      retStr += "\n" + work.talk();
       work.setAff( work.getAff() + 1 );
+      retStr += "\n+1 Affinity!";
     }
   }
   
-  public void library (Player A, Nerd ner, Student stu){
+  public String library (Player A, Nerd ner, Student stu){
   //(++int),smart,student(unable to raise interest if not route A)
+    String retStr = "";
     A.setInt( A.getInt() + 5 );
+    retStr += "Reading books gives you smarts. +5 Intelligence";
     int boo = (int) ( 5 * Math.random() );
     if ( boo == 0 && ner.getMet() == false ) {
       ner.setMet(true);
       //  ner.firstTalk();
     }
     else if (( boo == 0 )&&( ner.getMet()== true )) {
-      ner.talk();
+      retStr += "\n" + ner.talk();
       ner.setAff( ner.getAff() + 5 );
+      retStr += "\n+5 Affinity!";
     }
     else if ( ((int) ( 15 * Math.random() ) == 0) && (A.getRoute().contains("A")) ) {
-      stu.talk();
+      retStr += "\n" + stu.talk();
       stu.setAff( stu.getAff() + 1 );
+      retStr += "\n+1 Affinity!";
     }
+    return retStr;
   }
   
-  public void mall(Player A, Pretty pret, Workaholic work, Buffy buf){
+  public String mall(Player A, Pretty pret, Workaholic work, Buffy buf){
   //(+att , --$)pretty, work, buffy
+    String retStr = "";
     A.setAtt( A.getAtt() + 5 );
+    retStr += "You got prettier! +5 Attractiveness";
     A.setMoo( A.getMoo() - 10 );
+    retStr += "\nBut beauty comes at a price... - $10";
     int boo = (int) ( 5 * Math.random() );
     if ( (boo == 0) && (pret.getMet() == false )) {
       pret.setMet(true);
       // pret.firstTalk();
     }
     else if (( boo == 0 )&&( pret.getMet()== true) ) {
-      pret.talk();
+      retStr += "\n" + pret.talk();
       pret.setAff( pret.getAff() + 5 );
+      retStr += "\n+5 Affinity";
     }
     else if ( (int) ( 15 * Math.random() ) == 0  ) {
-      work.talk();
+      retStr += "\n" + work.talk();
       work.setAff( work.getAff() + 1 );
+      retStr += "\n+1 Affinity";
     }
     else if ( (int) ( 25 * Math.random() ) == 0  ) {
-      buf.talk();
+      retStr += "\n" + buf.talk();
       buf.setAff( buf.getAff() + 1 );
+      retStr += "\n+1 Affinity";
     }
+    return retStr;
   }
   
-  public void beautySalon(Player A, Pretty pret, Nerd ner){
+  public String beautySalon(Player A, Pretty pret, Nerd ner){
   //(++att, -$) pretty, smart
+    String retStr = "";
     A.setAtt( A.getStr() + 5 );
+    retStr += "You're feeling good, and looking even better! +5 Attractiveness";
     A.setMoo( A.getMoo() - 10 );
+    retStr += "\nYou look like a beauty, and pay like one too. - $10";
     int boo = (int) ( 10 * Math.random() );
     if ( boo == 0 ) {
-      pret.talk();
+      retStr += "\n" + pret.talk();
       pret.setAff( pret.getAff() + 1 );
+      retStr += 
     }
     else if ( (int) ( 25 * Math.random() ) == 0  ) {
       ner.talk();
